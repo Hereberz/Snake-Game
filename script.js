d43599139d981e50466f368413491abf6c6e4c28
Herebers <heber.joan@gmail.com>
2021-06-08 20:14:20 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0]={
     y:  8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen"
@@ -18,6 +22,11 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -37,7 +46,7 @@ function iniciarJogo(){                     //inicialização do jogo
     
     criarBG();
     criarCobrinha();
-
+    drawFood();
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
                                             //movimentação
